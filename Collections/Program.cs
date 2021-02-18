6ef5d48e803b5e662829a47cef606cc30c3fe096
KhayalT <teyyubovxeyal>
2021-02-18 14:40:18 +0400

    Collections and Generic with OOP

---
+++ Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] {
+            //"Engin","Murat","Kerem","Halit","Mustafa","Kaya"
+            //};
+            //Console.WriteLine(names[0]);
+   
+             
+            List<string> names2 = new List<string> {"Engin","Kaya","Mustafa" };
+            names2.Add("HHHH");
+            Console.WriteLine(names2[names2.Count-1]);
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " added.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " updated.");
+        }
+
+        public int Sum(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+
+        public void Sum2(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
